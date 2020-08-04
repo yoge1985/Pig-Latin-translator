2bad2bcc96337a4cd195e104df020de5f275862a
family gefen <jpgefen@gmail.com>
2020-08-04 23:09:05 +0100

    corrected error

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// prompts user to write a sentence and then the program converts the sentence into pig latin
-
+        String pigLatin;
         Scanner scanner = new Scanner(System.in);
         System.out.println("write a sentence:");
         String sentence = scanner.nextLine();
@@ -16,19 +16,31 @@ public class Main {
             String initialConsonants = str.replaceAll("([^aeiou]*).*","$1");
             int value = initialConsonants.length();
             String withoutInitialConsonant = str.substring(value);
-            String pigLatin = withoutInitialConsonant + initialConsonants + "ay";
+            pigLatin = withoutInitialConsonant + initialConsonants + "ay";
+            if (isVowel(str)){
+                pigLatin = str + "way";
+            }
 
             System.out.print(pigLatin + " ");
 
         }
-
 
 //        String[]arrayOfSentence = sentence.split(" ");
 
 //        for (String str : arrayOfSentence){
 //            String initialConsonants = str.replaceAll("([^aeiou]*).*","$1");
 //
-//        }
+//       }
         
+    }
+
+    private static boolean isVowel(String word){
+        if (word.charAt(0) == 'a' || word.charAt(0) =='e' || word.charAt(0)== 'i'
+            || word.charAt(0) == 'o' || word.charAt(0) == 'u'){
+            String pigLatin = word + "way";
+            return true;
+        }else {
+            return false;
+        }
     }
 }
